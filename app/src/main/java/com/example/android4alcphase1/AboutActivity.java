44262deed76db903fa6bbec45218347a05bbcd86
Mikhail Hathey <mikhail.hathey@gmail.com>
2019-07-15 23:32:33 +0200

    Fixed Webview

--- app/src/main/java/com/example/android4alcphase1/AboutActivity.java
+++ app/src/main/java/com/example/android4alcphase1/AboutActivity.java
@@ -5,6 +5,7 @@ import android.net.http.SslError;
 import android.os.Bundle;
 import android.view.View;
 import android.webkit.SslErrorHandler;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ProgressBar;
@@ -15,43 +16,61 @@ public class AboutActivity extends AppCompatActivity {
     WebView webView;
     ProgressBar progressBar;
 
-    String url = "https://www.andela.com/alc/";
-
     private static final String Act = "ProfileActivity";
-
-
-
-    public class MyWebClient extends WebViewClient{
-        @Override
-        public void onPageStarted(WebView view, String url, Bitmap favicon){
-            super.onPageStarted(view, url, favicon);
-        }
-
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url){
-            progressBar.setVisibility(View.VISIBLE);
-            return true;
-        }
-
-        @Override
-        public void onPageFinished(WebView view, String url){
-            super.onPageFinished(view, url);
-            progressBar.setVisibility(View.GONE);
-        }
-
-        @Override
-        public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error){
-            handler.proceed();
-        }
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.about_activity);
-        webView = (WebView) findViewById(R.id.webView);
-        progressBar = (ProgressBar) findViewById(R.id.progressBar);
-        webView.setWebViewClient(new MyWebClient());
-        webView.loadUrl(url);
+        webView = findViewById(R.id.webView);
+        progressBar = findViewById(R.id.progressBar);
+        progressBar.setMax(100);
+
+        webView.getSettings().setLoadWithOverviewMode(true);
+        webView.getSettings().setUseWideViewPort(true);
+        webView.getSettings().setBuiltInZoomControls(true);
+        webView.getSettings().setDisplayZoomControls(false);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
+        webView.setScrollbarFadingEnabled(false);
+
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+                handler.proceed();
+
+            }
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                progressBar.setVisibility(View.GONE);
+            }
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return super.shouldOverrideUrlLoading(view, url);
+            }
+
+        });
+        webView.loadUrl("https://andela.com/alc/");
     }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()){
+            webView.goBack();
+        }else {
+            finish();
+        }
+    }
+
 }
